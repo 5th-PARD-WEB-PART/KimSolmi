99c51439f4905a14414151a9b76bcfc65826a44b
thisissolmi <oksk6685@naver.com>
2025-04-18 19:42:33 +0900

    done

--- pard_5th_kimsolmi_instagram/src/pages/RegisterPage.tsx
+++ pard_5th_kimsolmi_instagram/src/pages/RegisterPage.tsx
@@ -16,8 +16,7 @@ export default function registerpage() {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-
-    // ✅ 입력값 필수 확인
+    //모든 필드를 다 입력하게 바꿨습니당~
     if (!phoneOrEmail || !fullName || !userName || !password) {
       alert('모든 필드를 입력해주세요.');
       return;
@@ -106,7 +105,9 @@ export default function registerpage() {
 
           <p className={styles.infoText}>
             저희 서비스를 이용하는 사람이 회원님의 연락처 정보를 Instagram에 업로드했을 수도 있습니다.{' '}
-            <a href="#" style={{ color: '#0095f6' }}>더 알아보기</a>
+            <Link href="#">
+  <span style={{ color: '#0095f6' }}>더 알아보기</span>
+</Link>
           </p>
 
           <button type="submit" className={styles.submitButton}>가입</button>
